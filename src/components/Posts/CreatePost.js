cedbadf77a31261d7ae446da5ed14db94d85eccf
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2022-10-01 11:49:21 +0530

    create post with sreekanth

--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -1,3 +1,4 @@
+import { useMemo, useRef } from 'react';
 import { useFormik } from 'formik';
 import DropZone from 'react-dropzone';
 import styled from 'styled-components';
@@ -6,6 +7,8 @@ import * as Yup from 'yup';
 import { createPostAction } from '../../redux/slices/posts/postSlices';
 import CategoriesOptions from '../Categories/CategoriesOptions';
 import { useNavigate } from 'react-router-dom';
+import JoditEditor from 'jodit-react';
+import RichTextEditor from 'react-rte';
 
 // Form schema
 const formSchema = Yup.object({
@@ -32,6 +35,7 @@ transition: border 0.24s ease-in-out;
 `;
 
 export default function CreatePost() {
+	const editor = useRef(null);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 
@@ -60,169 +64,320 @@ export default function CreatePost() {
 	});
 	if (isCreated) navigate('/posts');
 	return (
+		// <>
+		// 	<div className=" container min-h-screen  py-20 2xl:py-40 bg-white overflow-hidden  flex flex-col justify-center py-12 sm:px-6 lg:px-2">
+		// 		<div className="sm:mx-auto sm:w-full sm:max-w-md">
+		// 			<h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+		// 				Create a Blog here..
+		// 			</h1>
+		// 			<div className="mt-2 text-center text-sm text-gray-600">
+		// 				<p className="font-medium text-gray-600 hover:text-indigo-500">
+		// 					Share your ideas to the world. Your Blog must be
+		// 					free from profanity
+		// 				</p>
+		// 			</div>
+		// 			{appErr || serverErr ? (
+		// 				<div className="mt-2 text-center text-sm text-gray-600">
+		// 					<p className="font-lg text-red-600 ">
+		// 						{serverErr} {appErr}
+		// 					</p>
+		// 				</div>
+		// 			) : null}
+		// 		</div>
+		// 		<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+		// 			<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 border border-gray-300 ">
+		// 				<form
+		// 					onSubmit={formik.handleSubmit}
+		// 					className="space-y-6"
+		// 				>
+		// 					<div>
+		// 						<label
+		// 							htmlFor="title"
+		// 							className="block text-sm font-medium text-gray-700"
+		// 						>
+		// 							Title
+		// 						</label>
+		// 						<div className="mt-1">
+		// 							{/* Title */}
+		// 							<input
+		// 								value={formik.values.title}
+		// 								onChange={formik.handleChange('title')}
+		// 								onBlur={formik.handleBlur('title')}
+		// 								id="title"
+		// 								name="title"
+		// 								type="title"
+		// 								autoComplete="title"
+		// 								className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+		// 							/>
+		// 						</div>
+		// 						{/* Err msg */}
+		// 						<div className="text-red-500">
+		// 							{formik?.touched?.title &&
+		// 								formik?.errors?.title}
+		// 						</div>
+		// 					</div>
+		// 					<label
+		// 						htmlFor="category"
+		// 						className="block text-sm font-medium text-gray-700"
+		// 					>
+		// 						Category
+		// 					</label>
+		// 					<CategoriesOptions
+		// 						value={formik.values.category?.label}
+		// 						onChange={formik.setFieldValue}
+		// 						onBlur={formik.setFieldTouched}
+		// 						error={formik.errors.category}
+		// 						touched={formik.touched.category}
+		// 					/>
+		// 					<div>
+		// 						<label
+		// 							htmlFor="description"
+		// 							className="block text-sm font-medium text-gray-700"
+		// 						>
+		// 							Blog Content
+		// 						</label>
+		// 						{/* Description */}
+		// 						<JoditEditor
+		// 							value={formik.values.description}
+		// 							onChange={formik.handleChange(
+		// 								'description'
+		// 							)}
+		// 							onBlur={formik.handleBlur('description')}
+		// 							rows="20"
+		// 							cols="20"
+		// 							className="rounded-lg appearance-none block w-full py-3 px-3 text-base text-center leading-tight text-gray-600 bg-transparent focus:bg-transparent  border border-gray-200 focus:border-gray-500  focus:outline-none"
+		// 							type="text"
+		// 						/>
+		// 						{/* Image Component */}
+		// 						<label
+		// 							htmlFor="image"
+		// 							className="block text-sm font-medium mt-3 mb-2 text-gray-700"
+		// 						>
+		// 							Select Banner Image to your Blog
+		// 						</label>
+		// 						<Container className="container bg-gray-700">
+		// 							<DropZone
+		// 								onBlur={formik.handleBlur('image')}
+		// 								accept="image/jpeg, image/png image/webp"
+		// 								onDrop={(acceptedFiles) => {
+		// 									formik.setFieldValue(
+		// 										'image',
+		// 										acceptedFiles[0]
+		// 									);
+		// 								}}
+		// 							>
+		// 								{({ getRootProps, getInputProps }) => (
+		// 									<div className="container">
+		// 										<div
+		// 											{...getRootProps({
+		// 												className: 'dropzone',
+		// 												onDrop: (event) =>
+		// 													event.stopPropagation(),
+		// 											})}
+		// 										>
+		// 											<input
+		// 												{...getInputProps()}
+		// 											/>
+		// 											<p className="text-gray-300 text-lg cursor-pointer hover:text-gray-500">
+		// 												Click here to select
+		// 												Image
+		// 											</p>
+		// 										</div>
+		// 									</div>
+		// 								)}
+		// 							</DropZone>
+		// 						</Container>
+		// 						{/* Err msg */}
+		// 						<div className="text-red-500">
+		// 							{formik?.touched?.description &&
+		// 								formik?.errors?.description}
+		// 						</div>
+		// 					</div>
+		// 					<div>
+		// 						{/* Submit btn */}
+		// 						{loading ? (
+		// 							<button
+		// 								disabled
+		// 								type="submit"
+		// 								className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+		// 							>
+		// 								loading Please Wait...
+		// 							</button>
+		// 						) : (
+		// 							<button
+		// 								type="submit"
+		// 								className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+		// 							>
+		// 								publish
+		// 							</button>
+		// 						)}
+		// 					</div>
+		// 				</form>
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// </>
 		<>
-			<div className="min-h-screen  py-20 2xl:py-40 bg-white overflow-hidden">
-				<div className="container px-4 mx-auto">
-					<div className="sm:mx-auto sm:w-full sm:max-w-md">
-						<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-							Create Post
-						</h2>
-						<div className="mt-2 text-center text-sm text-gray-600">
-							<p className="font-medium text-gray-600 hover:text-indigo-500">
-								Share your ideas to the word. Your post must be
-								free from profanity
-							</p>
-						</div>
-						{appErr || serverErr ? (
-							<div className="mt-2 text-center text-sm text-gray-600">
-								<p className="font-lg text-red-600 ">
-									{serverErr} {appErr}
-								</p>
-							</div>
-						) : null}
-					</div>
-					<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-						<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 border border-gray-300 ">
-							<form
-								onSubmit={formik.handleSubmit}
-								className="space-y-6"
+			<div className="container min-h-screen bg-zinc-50 flex flex-col justify-center py-12 sm:px-6 lg:px-2">
+				<div className="sm:mx-auto sm:w-full sm:max-w-md ">
+					<h1 className=" text-center text-3xl font-extrabold text-blue-700 animate-bounce">
+						What going in Your Mind?...
+					</h1>
+
+					<p className="mt-2 text-center text-sm text-gray-600">
+						<p className="font-semibold text-gray-700 hover:text-indigo-500  ">
+							Share your ideas to the world. Your post must be
+							free from profanity
+						</p>
+					</p>
+				</div>
+				<div className="rounded-br-lg shadow-xl">
+					<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 ">
+						<form
+							onSubmit={formik.handleSubmit}
+							className="space-y-6"
+						>
+							{/* Category input goes here */}
+
+							<label
+								htmlFor="password"
+								className="block text-sm font-bold text-indigo-600"
 							>
-								<div>
-									<label
-										htmlFor="title"
-										className="block text-sm font-medium text-gray-700"
-									>
-										Title
-									</label>
-									<div className="mt-1">
-										{/* Title */}
-										<input
-											value={formik.values.title}
-											onChange={formik.handleChange(
-												'title'
-											)}
-											onBlur={formik.handleBlur('title')}
-											id="title"
-											name="title"
-											type="title"
-											autoComplete="title"
-											className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-										/>
-									</div>
-									{/* Err msg */}
-									<div className="text-red-500">
-										{formik?.touched?.title &&
-											formik?.errors?.title}
-									</div>
-								</div>
+								Select Category
+							</label>
+
+							<CategoriesOptions
+								value={formik.values.category?.label}
+								onChange={formik.setFieldValue}
+								onBlur={formik.setFieldTouched}
+								error={formik.errors.category}
+								touched={formik.touched.category}
+							/>
+
+							<div>
 								<label
-									htmlFor="category"
-									className="block text-sm font-medium text-gray-700"
+									htmlFor="email"
+									className="block text-sm font-bold text-indigo-600"
 								>
-									Category
+									Title
 								</label>
-								<CategoriesOptions
-									value={formik.values.category?.label}
-									onChange={formik.setFieldValue}
-									onBlur={formik.setFieldTouched}
-									error={formik.errors.category}
-									touched={formik.touched.category}
-								/>
-								<div>
-									<label
-										htmlFor="description"
-										className="block text-sm font-medium text-gray-700"
-									>
-										Description
-									</label>
-									{/* Description */}
-									<textarea
-										value={formik.values.description}
-										onChange={formik.handleChange(
-											'description'
-										)}
-										onBlur={formik.handleBlur(
-											'description'
-										)}
-										rows="5"
-										cols="10"
-										className="rounded-lg appearance-none block w-full py-3 px-3 text-base text-center leading-tight text-gray-600 bg-transparent focus:bg-transparent  border border-gray-200 focus:border-gray-500  focus:outline-none"
-										type="text"
-									></textarea>
-									{/* Image Component */}
-									<label
-										htmlFor="image"
-										className="block text-sm font-medium mt-3 mb-2 text-gray-700"
-									>
-										Select image to upload
-									</label>
-									<Container className="container bg-gray-700">
-										<DropZone
-											onBlur={formik.handleBlur('image')}
-											accept="image/jpeg, image/png image/webp"
-											onDrop={(acceptedFiles) => {
-												formik.setFieldValue(
-													'image',
-													acceptedFiles[0]
-												);
-											}}
-										>
-											{({
-												getRootProps,
-												getInputProps,
-											}) => (
-												<div className="container">
-													<div
-														{...getRootProps({
-															className:
-																'dropzone',
-															onDrop: (event) =>
-																event.stopPropagation(),
-														})}
-													>
-														<input
-															{...getInputProps()}
-														/>
-														<p className="text-gray-300 text-lg cursor-pointer hover:text-gray-500">
-															Click here to select
-															Image
-														</p>
-													</div>
-												</div>
-											)}
-										</DropZone>
-									</Container>
-									{/* Err msg */}
-									<div className="text-red-500">
-										{formik?.touched?.description &&
-											formik?.errors?.description}
-									</div>
+								<div className="mt-1">
+									{/* Title */}
+									<input
+										value={formik.values.title}
+										onChange={formik.handleChange('title')}
+										onBlur={formik.handleBlur('title')}
+										id="title"
+										name="title"
+										type="title"
+										autoComplete="title"
+										className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+									/>
 								</div>
-								<div>
-									{/* Submit btn */}
-									{loading ? (
-										<button
-											disabled
-											type="submit"
-											className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-										>
-											loading Please Wait...
-										</button>
-									) : (
-										<button
-											type="submit"
-											className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-										>
-											Create
-										</button>
+								{/* Err msg */}
+								<div className="text-red-500">
+									{formik?.touched?.title &&
+										formik?.errors?.title}
+								</div>
+							</div>
+
+							<div>
+								<label
+									htmlFor="password"
+									className="block text-bold font-medium text-indigo-700"
+								>
+									Post Content
+								</label>
+								{/* Description */}
+								<JoditEditor
+									ref={editor}
+									value={formik.values.description}
+									onChange={formik.handleChange(
+										'description'
 									)}
+									onBlur={formik.handleBlur('description')}
+									rows="5"
+									cols="10"
+									className="rounded-lg appearance-none block w-full py-3 px-3 text-base text-center leading-tight text-gray-600 bg-transparent focus:bg-transparent  border border-gray-200 focus:border-gray-500  focus:outline-none"
+									type="text"
+								/>
+								{/* Err msg */}
+								<div className="text-red-500">
+									{formik?.touched?.description &&
+										formik?.errors?.description}
 								</div>
-							</form>
-						</div>
+
+								{/* Image component */}
+								<label
+									htmlFor="password"
+									className="block text-sm font-medium mt-3 mb-2 text-indigo-700"
+								>
+									Select image to upload
+								</label>
+								<Container className="container bg-gray-700">
+									<DropZone
+										onBlur={formik.handleBlur('image')}
+										accept="image/jpeg, image/png image/webp"
+										onDrop={(acceptedFiles) => {
+											formik.setFieldValue(
+												'image',
+												acceptedFiles[0]
+											);
+										}}
+									>
+										{({ getRootProps, getInputProps }) => (
+											<div className="container">
+												<div
+													{...getRootProps({
+														className: 'dropzone',
+														onDrop: (event) =>
+															event.stopPropagation(),
+													})}
+												>
+													<input
+														{...getInputProps()}
+													/>
+													<p className="text-gray-300 text-lg cursor-pointer hover:text-gray-500">
+														Click here to select
+														image
+													</p>
+												</div>
+											</div>
+										)}
+									</DropZone>
+								</Container>
+
+								{appErr || serverErr ? (
+									<p className="mt-2 text-center text-lg text-red-600">
+										{serverErr} {appErr}
+									</p>
+								) : null}
+							</div>
+							<div>
+								{/* Submit btn */}
+								{loading ? (
+									<button
+										disabled
+										className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										Loading please wait...
+									</button>
+								) : (
+									<button
+										type="submit"
+										className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-xl text-sm font-bold text-white bg-gradient-to-r from-green-500 to-blue-500 hover:from-pink-600 hover:to-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 "
+									>
+										Publish
+									</button>
+								)}
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
-			{/* </div> */}
+			{/* <div className="container">
+      <NewFeed />
+      </div> */}
 		</>
 	);
 }
